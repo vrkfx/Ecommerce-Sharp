1af044c6503b01e39958de838a8ea17ef1667d74
khaido51 <dokhai92@gmail.com>
2023-03-22 11:22:25 +1030

    Login And Signup features
    Commit

---
+++ MVCEcommerce/MVCEcommerce/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCEcommerce.Data;
+
+namespace MVCEcommerce.Controllers
+{
+	public class LoginController : Controller
+	{
+
+		private readonly MVCEcommerceContext _context;
+
+		public LoginController(MVCEcommerceContext context)
+		{
+			_context = context;	
+		}
+		//public async Task<IActionResult> Index(string inputeEmail, string inputPassword)
+		//{
+		//	if (_context.User == null)
+		//	{
+		//		return NotFound();
+		//	}
+		//}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Signup()
+		{
+			return View();
+		}
+	}
+}
